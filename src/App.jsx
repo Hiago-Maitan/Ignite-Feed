4685ef07800fe5d87a93efb394274c9341f3f2d8
Maitann <hiagomaitan@gmail.com>
2024-02-28 14:43:50 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header"
+import './global.css'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>ola</h1>
+    </>
+  )
+}
+
+export default App
